744a3823cd773f462ed35df3d2bbdb24049d3656
Fernando García Hansen <fgarcia@humanoide.es>
2026-05-05 12:20:08 +0200

    Update update_all_modules.sh

--- update_all_modules.sh
+++ update_all_modules.sh
@@ -1,7 +1,7 @@
-docker exec -it odoo18-web-1 bash -c "
+docker exec -it odoo19-web-1 bash -c "
 odoo --no-http --workers=0 --max-cron-threads=0 \
 --load=base,web \
---db_host=odoo18-db-1 \
+--db_host=odoo19-db-1 \
 --db_user=odoo \
 --db_password=odoo \
 --stop-after-init \
